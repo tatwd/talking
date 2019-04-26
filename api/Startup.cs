754a9eee1c050a959aaef9d9739334e4665a6497
tatwd <leoking9641@hotmail.com>
2019-04-26 16:28:08 +0800

    refactor: remove comments

--- api/Startup.cs
+++ api/Startup.cs
@@ -26,7 +26,6 @@ namespace Talking.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -36,11 +35,9 @@ namespace Talking.Api
                 options.DatabaseName = "talking_sys";
             });
 
-            services.AddTransient<IUserRepository, UserRepository>();
             services.AddTransient<ICommentRepository, CommentRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -49,7 +46,6 @@ namespace Talking.Api
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
